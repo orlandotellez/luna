ae217dfa86f4176ce4977ce7be2a52aeb86eee3c
orlandotellez <espadachinkinno@gmail.com>
2026-07-10 10:05:44 -0600

    feat: agregar el servicio de autenticacion, creado en metodo de registrar usuario

---
+++ backend/src/Luna.Application/Features/Auth/AuthService.cs
@@ -0,0 +1,93 @@
+using Luna.Application.Common.Interfaces;
+using Luna.Application.Common.Models;
+using Luna.Application.Common.Mapping;
+using Luna.Domain.Exceptions;
+using Luna.Domain.Entities;
+
+namespace Luna.Application.Features.Auth;
+
+public class AuthService : IAuthService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IAccountRepository _accountRepository;
+    private readonly ISessionRepository _sessionRepository;
+    private readonly IPasswordService _passwordService;
+    private readonly ITokenService _tokenService;
+
+    public AuthService(
+        IUserRepository userRepository,
+        IAccountRepository accountRepository,
+        ISessionRepository sessionRepository,
+        IPasswordService passwordService,
+        ITokenService tokenService
+        )
+    {
+        _userRepository = userRepository;
+        _accountRepository = accountRepository;
+        _sessionRepository = sessionRepository;
+        _passwordService = passwordService;
+        _tokenService = tokenService;
+    }
+
+    public async Task<AuthResponse> RegisterAsync(RegisterRequest request)
+    {
+        // Verify email
+        var existingUser = await _userRepository.GetByEmailAsync(request.Email);
+        if (existingUser != null) throw AppExceptions.Conflict("Email already registered");
+
+        // Hash password 
+        var hashedPassword = _passwordService.HashPassword(request.Password);
+
+        // Create user
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = request.Name,
+            Role = "standar",
+            IsActive = true,
+            EmailVerified = false,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+        await _userRepository.CreateAsync(user);
+
+        // create credentials account
+        var account = new Account
+        {
+            Id = Guid.NewGuid(),
+            AccountId = user.Id.ToString(),
+            ProviderId = "credentials",
+            UserId = user.Id,
+            Password = hashedPassword,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+        await _accountRepository.CreateAsync(account);
+
+        // Generater token JWT
+        var (accessToken, refreshToken) = _tokenService.GenerateTokens(user.Id, user.Email, user.Role);
+
+        // Create session
+        var session = new Session
+        {
+            Id = Guid.NewGuid(),
+            UserId = user.Id,
+            Token = refreshToken,
+            ExpiresAt = DateTime.UtcNow.AddDays(7),
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+        await _sessionRepository.CreateAsync(session);
+
+        return new AuthResponse
+        {
+            Message = "User created sucessfully. Please veriry your email",
+            User = user.MapUserToDto(),
+            AccessToken = accessToken,
+            RefreshToken = refreshToken
+        };
+    }
+}
