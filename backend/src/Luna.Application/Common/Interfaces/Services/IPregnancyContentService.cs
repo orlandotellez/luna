3199a926e83e00cf10cef9e252e960ab90d22744
orlandotellez <espadachinkinno@gmail.com>
2026-07-20 21:04:08 -0600

    feat(api): agregar endpoint para conocer la info detallada de una semana en especifico del embarazo(todo mock)

---
+++ backend/src/Luna.Application/Common/Interfaces/Services/IPregnancyContentService.cs
@@ -0,0 +1,9 @@
+using Luna.Application.Common.Models.Pregnancy;
+
+namespace Luna.Application.Common.Interfaces.Services;
+
+public interface IPregnancyContentService
+{
+    Task<PregnancyWeekContentDto?> GetWeekAsync(int weekNumber);
+    Task<List<PregnancyWeekContentDto>> GetAllWeeksAsync();
+}
