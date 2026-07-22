54434f4e7cb068bd5aafe075915f0af6f51e0b73
orlandotellez <espadachinkinno@gmail.com>
2026-07-22 12:29:08 -0600

    chore(tipos): agregar carpeta para datos y tipos en el frontend

---
+++ mobile-app/src/shared/data/user.ts
@@ -0,0 +1,10 @@
+import { User } from "../types";
+
+export const currentUser: User = {
+  id: '1',
+  name: 'Maria Lopez',
+  email: 'maria@correo.com',
+  avatarInitials: 'M',
+  stage: 'cycle',
+  stageLabel: 'Ciclo Menstrual Activo',
+};
